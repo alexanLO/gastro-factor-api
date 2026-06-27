e665e3b00e9b252df265c8e967d41d9d6c827014
zAlle <alexanlima1.0@gmail.com>
2026-06-26 21:32:29 -0300

    Criando projeto

---
+++ src/main/java/br/com/gastrofactorapi/GastrofactorapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gastrofactorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrofactorapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastrofactorapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gastrofactorapi/GastrofactorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gastrofactorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastrofactorapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
